b0e630411c07eb412e9c5e0c416ce49c7f91ac15
PRIYANKA-Nigam <nigampriyanka042@gmail.com>
2021-08-15 12:36:05 +0530

    spin

---
+++ app/src/main/java/com/example/coloredspinner/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.coloredspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+Spinner s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        s=findViewById(R.id.sp);
+        ArrayAdapter adapter=ArrayAdapter.createFromResource(this,R.array.Spinner_Items,R.layout.colors);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
+        s.setAdapter(adapter);
+        s.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this,parent.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
